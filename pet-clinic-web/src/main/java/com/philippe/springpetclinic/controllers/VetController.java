ffef0a4bf68fae58406b09aabbeed467a55b674f
Philippe Peelman <philippe.peelman@axians.com>
2020-01-17 16:39:36 +0100

    adding vets page and controller. closes #6

--- pet-clinic-web/src/main/java/com/philippe/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/philippe/springpetclinic/controllers/VetController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class indexController {
+public class VetController {
 
-    @GetMapping({"", "/", "index", "index.html"})
-    public String index() {
-        return "index";
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
     }
 }
